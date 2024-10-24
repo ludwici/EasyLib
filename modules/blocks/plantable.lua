cc63270a97ef4dc21da99d87dfeee56c0e9e8404
ludwici <gammaclone@gmail.com>
2024-10-24 05:20:01 +0300

    Save block states to object

--- modules/blocks/plantable.lua
+++ modules/blocks/plantable.lua
@@ -2,21 +2,29 @@ local EasyLib = require "easylib:main"
 
 local Plantable = {}
 
-function Plantable:get_drop_table()
+Plantable.states = {}
+
+function Plantable:get_max_age()
+    return 7
+end
+
+function Plantable:get_drop_table(x, y, z)
     return {}
 end
 
 function Plantable:get_base_block_name()
-    return "simplefarming:wheat_stage"
+    return nil
 end
 
-function Plantable:grow_plant(x, y, z, base_block_name)
+function Plantable:update_states(x, y, z)
+    self.states = block.get_states(x, y, z)
+end
+
+function Plantable:grow_plant(x, y, z)
     local blockid = block.get(x, y, z)
     local blockName = block.name(blockid)
 
-    if not base_block_name then
-        base_block_name = self:get_base_block_name()
-    end
+    local base_block_name = self:get_base_block_name()
 
     if not base_block_name then
         return
@@ -25,32 +33,36 @@ function Plantable:grow_plant(x, y, z, base_block_name)
     local states = block.get_states(x, y, z)
     local dec = block.decompose_state(states)
 
-    local age = math.min(dec[3] + 1, self:get_max_age())
+    local age = dec[3] + 1
+    if age > self:get_max_age() then
+        age = self:get_max_age()
+        return false
+    end
+
+    local block_index = block.index(base_block_name..age)
+
+    if not block_index then
+        return false
+    end
+
     dec[3] = age
     block.set_user_bits(x, y, z, 0, 1, age)
     local newState = block.compose_state(dec)
     -- print(x, y, z, blockName.." modified to "..base_block_name..age, dec[3], states, newState)
-    block.place(x, y, z, block.index(base_block_name..age), newState)
+
+    block.place(x, y, z, block_index, newState)
+    return true
 end
 
-function Plantable:on_interact(x, y, z, playerid)
-    local age = self:get_age(x, y, z)
-    if age == self:get_max_age() then
-        local ppos = vec3.add({x, y, z}, {0.5, 0.0, 0.5})
-        EasyLib:drop_items(ppos, self)
-        block.set(x, y, z, block.index("core:air"), 0)
-        return true
-    end
+function Plantable:on_broken(x, y, z, playerid)
+    local ppos = vec3.add({x, y, z}, {0.5, 0.0, 0.5})
+    EasyLib:drop_items(ppos, self, x, y, z)
+    block.set(x, y, z, block.index("core:air"), 0)
 end
 
-function Plantable:get_age(x, y, z)
-    local states = block.get_states(x, y, z)
-    local dec = block.decompose_state(states)
+function Plantable:get_age()
+    local dec = block.decompose_state(self.states)
     return dec[3]
-end
-
-function Plantable:get_max_age()
-    return 7
 end
 
 return Plantable
